ee096908a6daa0803a5dc357d61ea971b507ac77
Bogdan Cristea <cristeab@gmail.com>
2024-04-28 00:56:59 +0300

    sign pkg installer

--- build-macos-release.sh
+++ build-macos-release.sh
@@ -1,76 +1,73 @@
 #!/bin/bash
 
+set -e # exit immeditalley on error
+
 QT_VER=6.7.0
 APP_NAME=EvidentaFiscala
 APP_IDENTIFIER="com.cristeab.finance"
 MAJOR_VERSION=1.0
 MINOR_VERSION=$(git rev-list --count HEAD)
+APP_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}"
+
+DEVELOPER_ID="Bogdan Cristea"
+DEVELOPER_USERNAME="cristeab@gmail.com"
+TEAM_ID="FAARUB626Q"
+INSTALL_LOCATION="/Applications"
+PKG_NAME=$APP_NAME-$APP_VERSION
 
 echo "QT version $QT_VER"
 
 rm -rf build
 
 mkdir build
-cd build
+pushd build
 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$HOME/Qt/$QT_VER/macos
-make -j
-make pack
-cd ..
+cmake --build . -j
+cmake --build . --target pack
+
+popd
 
 # sign bundle AFTER macdeploy
 codesign --strict --timestamp --force --verify --verbose --deep \
           --entitlements ./Entitlements.plist \
-          --sign "Developer ID Application: Bogdan Cristea" \
+          --sign "Developer ID Application: $DEVELOPER_ID" \
           --options runtime ./build/$APP_NAME.app
 
- # check the signing
- codesign --verify --verbose=4 --deep --strict ./build/$APP_NAME.app
- if [ $? -ne 0 ]; then
-     exit $?
- fi
+# check the signing
+codesign --verify --verbose=4 --deep --strict ./build/$APP_NAME.app
 
- # create installer
- rm -f build/$APP_NAME.dmg
- $HOME/node_modules/appdmg/bin/appdmg.js CustomDmg.json build/$APP_NAME.dmg
+# create installer
+pkgbuild --analyze --root build/$APP_NAME.app Components.plist
 
- if [ -z "$APP_PWD" ]; then
+pkgbuild --identifier $APP_IDENTIFIER \
+         --version $APP_VERSION \
+         --root build/$APP_NAME.app \
+         --component-plist Components.plist \
+         --install-location $INSTALL_LOCATION/$APP_NAME.app \
+         build/$PKG_NAME.pkg
+
+productbuild --synthesize --package build/$PKG_NAME.pkg distribution.xml
+
+cp build/$PKG_NAME.pkg .
+productbuild --distribution distribution.xml \
+             --resources . \
+             build/$PKG_NAME.pkg
+rm $PKG_NAME.pkg
+
+if [ -z "$APP_PWD" ]; then
      echo Notarization password is not set
      exit 0
- fi
+fi
 
- echo "Upload installer to Apple servers"
- xcrun altool --notarize-app \
-              --primary-bundle-id $APP_IDENTIFIER \
-              --username cristeab@gmail.com \
-              --password $APP_PWD \
-              --asc-provider "FAARUB626Q" \
-              --file ./build/$APP_NAME.dmg | tee build/notarization.log
- UUID=`cat build/notarization.log | grep -Eo '\w{8}-(\w{4}-){3}\w{12}$'`
- if [ $? -ne 0 ]; then
-     exit $?
- fi
+echo "Upload installer to Apple servers"
+xcrun notarytool submit build/$PKG_NAME.pkg \
+             --apple-id $DEVELOPER_USERNAME \
+             --team-id $TEAM_ID \
+             --password $APP_PWD \
+             --verbose \
+             --wait
 
- echo -n "Check notarization result "
- while true; do
-     xcrun altool --notarization-info $UUID \
-                  --username cristeab@gmail.com \
-                  --password $APP_PWD &> build/notarization.log
-     r=`cat build/notarization.log`
-     t=`echo "$r" | grep "success"`
-     f=`echo "$r" | grep "invalid"`
-     if [[ "$t" != "" ]]; then
-         break
-     fi
-     if [[ "$f" != "" ]]; then
-         echo "failure"
-         echo "$r"
-         exit 1
-     fi
-     echo -n "."
-     sleep 30
- done
- echo "success"
-
- # once the notarization is successful
- xcrun stapler staple -v build/$APP_NAME.dmg
+# once the notarization is successful
+echo "Staple the installer"
+xcrun stapler staple -v build/$APP_NAME.dmg
