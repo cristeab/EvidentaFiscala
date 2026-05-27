2a47344694e66f4ce59a895faab91eef583a47bb
Bogdan Cristea <cristeab@gmail.com>
2026-05-27 23:46:00 +0300

    Update French and Romanian localization strings
    * Add translations for new `ContextMenu` and `LabelTextFieldBrowser` components.
    * Rename 'Observations' to 'Comments' and provide new related strings in `ContentView`.
    * Introduce new settings translations for Git backup, currency conversion options, and general backup validations.
    * Include new prompts for ledger creation and relocated general application messages.
    * Adjust string location references due to ongoing UI refactoring.
    * Closes #42

--- langs/ro.ts
+++ langs/ro.ts
@@ -4,54 +4,76 @@
 <context>
     <name>ContentView</name>
     <message>
-        <location filename="../qml/ContentView.qml" line="37"/>
+        <location filename="../qml/ContentView.qml" line="45"/>
         <source>Date</source>
         <translation>Data</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="48"/>
+        <location filename="../qml/ContentView.qml" line="56"/>
         <source>Amount</source>
         <translation>Sumă</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="63"/>
+        <location filename="../qml/ContentView.qml" line="76"/>
         <source>Exchange Rate</source>
         <translation>Rată de schimb</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="105"/>
         <source>Observations</source>
-        <translation>Observații</translation>
+        <translation type="vanished">Observații</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="110"/>
+        <location filename="../qml/ContentView.qml" line="119"/>
+        <source>Comments</source>
+        <translation>Comentarii</translation>
+    </message>
+    <message>
+        <location filename="../qml/ContentView.qml" line="157"/>
         <source>Add</source>
         <translation>Adaugă</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="118"/>
+        <location filename="../qml/ContentView.qml" line="165"/>
         <source>Date must be specified</source>
         <translation>Data trebuie specificată</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="123"/>
+        <location filename="../qml/ContentView.qml" line="170"/>
         <source>Amount must be specified</source>
         <translation>Suma trebuie specificată</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="128"/>
+        <location filename="../qml/ContentView.qml" line="175"/>
         <source>Exchange rate must be specified</source>
         <translation>Rata de schimb trebuie specificată</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="133"/>
+        <location filename="../qml/ContentView.qml" line="180"/>
         <source>Observations must be specified</source>
         <translation>Observațiile trebuie specificate</translation>
     </message>
     <message>
-        <location filename="../qml/ContentView.qml" line="149"/>
+        <location filename="../qml/ContentView.qml" line="195"/>
         <source>Cannot add a new row</source>
         <translation>Nu se poate adăuga un rând nou</translation>
+    </message>
+</context>
+<context>
+    <name>ContextMenu</name>
+    <message>
+        <location filename="../qml/ContextMenu.qml" line="19"/>
+        <source>Delete row #</source>
+        <translation>Șterge rândul #</translation>
+    </message>
+    <message>
+        <location filename="../qml/ContextMenu.qml" line="30"/>
+        <source>Question</source>
+        <translation>Întrebare</translation>
+    </message>
+    <message>
+        <location filename="../qml/ContextMenu.qml" line="31"/>
+        <source>Are you sure you want to delete row #</source>
+        <translation>Sigur doriți să ștergeți rândul #</translation>
     </message>
 </context>
 <context>
@@ -108,16 +130,29 @@ Cheltuieli: %3</translation>
     </message>
 </context>
 <context>
+    <name>LabelTextFieldBrowser</name>
+    <message>
+        <location filename="../qml/LabelTextFieldBrowser.qml" line="38"/>
+        <source>Select </source>
+        <translation>Selectați </translation>
+    </message>
+</context>
+<context>
     <name>Settings</name>
     <message>
-        <location filename="../qml/Settings.qml" line="14"/>
+        <location filename="../qml/Settings.qml" line="12"/>
         <source>Settings</source>
         <translation>Setări</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="25"/>
+        <location filename="../qml/Settings.qml" line="23"/>
         <source>Invalid Settings</source>
         <translation>Setări invalide</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="28"/>
+        <source>Backup folder path cannot be empty</source>
+        <translation>Calea folderului de backup nu poate fi goală</translation>
     </message>
     <message>
         <location filename="../qml/Settings.qml" line="53"/>
@@ -130,50 +165,105 @@ Cheltuieli: %3</translation>
         <translation>Coloane vizibile</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="82"/>
+        <location filename="../qml/Settings.qml" line="59"/>
+        <source>Backup</source>
+        <translation>Backup</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="62"/>
+        <source>Currency</source>
+        <translation>Monedă</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="98"/>
         <source>Minimum Gross Income</source>
         <translation>Venitul brut minim</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="89"/>
+        <location filename="../qml/Settings.qml" line="105"/>
         <source>Graphic Representation</source>
         <translation>Reprezentare grafică</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="90"/>
+        <location filename="../qml/Settings.qml" line="106"/>
         <source>Bars</source>
         <translation>Bare</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="90"/>
+        <location filename="../qml/Settings.qml" line="106"/>
         <source>Lines</source>
         <translation>Linii</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="97"/>
+        <location filename="../qml/Settings.qml" line="113"/>
         <source>Working Directory</source>
         <translation>Director de lucru</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="103"/>
+        <location filename="../qml/Settings.qml" line="122"/>
         <source>Starting Number for Receipts</source>
         <translation>Număr de pornire pentru chitanțe</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="112"/>
+        <location filename="../qml/Settings.qml" line="131"/>
         <source>Interface Language</source>
         <translation>Limba interfeței</translation>
     </message>
     <message>
-        <location filename="../qml/Settings.qml" line="119"/>
+        <location filename="../qml/Settings.qml" line="138"/>
         <source>CSV Header Language</source>
         <translation>Limba antetului CSV</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="161"/>
+        <source>Restart the application to apply changes</source>
+        <translation>Reporniți aplicația pentru a aplica modificările</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="185"/>
+        <source>Enable Row Numbers</source>
+        <translation>Activare numerotare rânduri</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="207"/>
+        <source>Git Backup</source>
+        <translation>Backup Git</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="214"/>
+        <source>Backup Directory</source>
+        <translation>Director de backup</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="224"/>
+        <source>Username</source>
+        <translation>Nume utilizator</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="231"/>
+        <source>User Email</source>
+        <translation>E-mail utilizator</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="254"/>
+        <source>Currency Conversion</source>
+        <translation>Conversie valutară</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="260"/>
+        <source>Exchange Rate Provider</source>
+        <translation>Furnizor de curs valutar</translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings.qml" line="261"/>
+        <source>National Bank of Romania</source>
+        <translation>Banca Națională a României</translation>
     </message>
 </context>
 <context>
     <name>TableModel</name>
     <message>
-        <location filename="../src/table_model.cpp" line="33"/>
+        <location filename="../src/table_model.cpp" line="32"/>
         <source>Date</source>
         <translation>Data</translation>
     </message>
@@ -183,89 +273,97 @@ Cheltuieli: %3</translation>
         <translation>Venit bancar</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="33"/>
+        <location filename="../src/table_model.cpp" line="34"/>
         <source>Cash Income</source>
         <translation>Venit în numerar</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="34"/>
+        <location filename="../src/table_model.cpp" line="35"/>
         <source>Bank Expenses</source>
         <translation>Cheltuieli bancare</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="34"/>
+        <location filename="../src/table_model.cpp" line="36"/>
         <source>Cash Expenses</source>
         <translation>Cheltuieli în numerar</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="35"/>
+        <location filename="../src/table_model.cpp" line="37"/>
         <source>Invoice Number</source>
         <translation>Număr factură</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="35"/>
+        <location filename="../src/table_model.cpp" line="38"/>
         <source>Comments</source>
         <translation>Comentarii</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="57"/>
+        <location filename="../src/table_model.cpp" line="56"/>
         <source>CSV file name is empty</source>
         <translation>Numele fișierului CSV este gol</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="65"/>
+        <location filename="../src/table_model.cpp" line="64"/>
         <source>CSV file cannot be created</source>
         <translation>Fișierul CSV nu poate fi creat</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="73"/>
+        <location filename="../src/table_model.cpp" line="75"/>
         <source>CSV file does not have the expected columns</source>
         <translation>Fișierul CSV nu are coloanele așteptate</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="83"/>
+        <location filename="../src/table_model.cpp" line="85"/>
         <source>CSV file has a different number of columns than expected, %1</source>
         <translation>Fișierul CSV are un număr diferit de coloane față de cel așteptat, %1</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="207"/>
+        <location filename="../src/table_model.cpp" line="208"/>
+        <location filename="../src/table_model.cpp" line="233"/>
         <source>Cannot write CSV file {}</source>
         <translation>Nu se poate scrie fișierul CSV {}</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="511"/>
-        <source>Cannot write to </source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../src/table_model.cpp" line="565"/>
         <source>Restart the application to apply changes</source>
-        <translation>Reporniți aplicația pentru a aplica modificările</translation>
+        <translation type="vanished">Reporniți aplicația pentru a aplica modificările</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="584"/>
+        <location filename="../src/table_model.cpp" line="463"/>
         <source>Invalid number of columns in candidate row</source>
         <translation>Număr invalid de coloane în rândul candidat</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="616"/>
+        <location filename="../src/table_model.cpp" line="495"/>
         <source>Invalid date in candidate row</source>
         <translation>Dată invalidă în rândul candidat</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="630"/>
+        <location filename="../src/table_model.cpp" line="509"/>
         <source>Multiple transactions in candidate row</source>
         <translation>Mai multe tranzacții în rândul candidat</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="636"/>
+        <location filename="../src/table_model.cpp" line="515"/>
         <source>No transaction amount in candidate row</source>
         <translation>Nicio sumă de tranzacție în rândul candidat</translation>
     </message>
     <message>
-        <location filename="../src/table_model.cpp" line="670"/>
+        <location filename="../src/table_model.cpp" line="549"/>
         <source>Found possible duplicate row #%1</source>
         <translation>S-a găsit posibil duplicat rândul #%1</translation>
+    </message>
+</context>
+<context>
+    <name>UiController</name>
+    <message>
+        <location filename="../src/ui_controller.cpp" line="76"/>
+        <source>Cannot write to </source>
+        <translation>Nu se poate scrie în </translation>
+    </message>
+    <message>
+        <location filename="../src/ui_controller.cpp" line="319"/>
+        <source>Cannot create a new file name %1</source>
+        <translation>Nu se poate crea un nou fișier cu numele %1</translation>
     </message>
 </context>
 <context>
@@ -276,54 +374,68 @@ Cheltuieli: %3</translation>
         <translation>Înregistrări fiscale</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="28"/>
+        <location filename="../qml/main.qml" line="33"/>
         <source>Open...</source>
         <translation>Deschide...</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="39"/>
+        <location filename="../qml/main.qml" line="52"/>
         <source>Settings...</source>
         <translation>Setări...</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="48"/>
+        <location filename="../qml/main.qml" line="42"/>
         <source>Generate Fiscal Records Register</source>
         <translation>Generează registrul fiscal</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="66"/>
+        <location filename="../qml/main.qml" line="67"/>
+        <source>Question</source>
+        <translation>Întrebare</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="68"/>
+        <source>Create new ledger </source>
+        <translation>Creează un registru nou </translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="78"/>
+        <source>New CSV File...</source>
+        <translation>Fișier CSV nou…</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="95"/>
         <source>Table</source>
         <translation>Tabel</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="69"/>
+        <location filename="../qml/main.qml" line="98"/>
         <source>Chart</source>
         <translation>Grafic</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="135"/>
+        <location filename="../qml/main.qml" line="164"/>
         <source>Click to Open in Default Editor</source>
         <translation>Faceți clic pentru a deschide în editorul implicit</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="156"/>
+        <location filename="../qml/main.qml" line="186"/>
         <source>Error</source>
         <translation>Eroare</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="156"/>
+        <location filename="../qml/main.qml" line="186"/>
         <source>Warning</source>
         <translation>Avertisment</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="168"/>
+        <location filename="../qml/main.qml" line="241"/>
         <source>Select File</source>
         <translation>Selectează fișierul</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="186"/>
         <source>Select Working Directory</source>
-        <translation>Selectează directorul de lucru</translation>
+        <translation type="vanished">Selectează directorul de lucru</translation>
     </message>
 </context>
 </TS>
